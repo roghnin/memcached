1d02bfce2883226d20265013fa627e0057c1b028
Haosen Wen <hwen5@node2x20a.cs.rochester.edu>
2022-07-02 22:08:53 -0400

    set StrictEpochBoundary to true to avoid old-see-new issue in Montage

--- memcached.c
+++ memcached.c
@@ -5923,6 +5923,7 @@ int main (int argc, char **argv) {
         // should have the old base in 'meta' as of here.
     }
 
+    montage_set_env("StrictEpochBoundary", "true");
     montage_set_env("EpochLength", "10");
     montage_set_env("EpochLengthUnit", "Microsecond");
     montage_init(MONTAGE_THREAD_CNT);
